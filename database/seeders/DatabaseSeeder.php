ed10933683ecd98d4d50d3664815bef06b7d5feb
Raymond Youssef <rimonomega@gmail.com>
2021-02-24 16:40:17 +0200

    first build

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ProductSeeder::class,
             CategorySeeder::class,
-            ArticleSeeder::class
+            ArticleSeeder::class,
+            UserSeeder::class
         ]);
         // \App\Models\User::factory(10)->create();
     }
