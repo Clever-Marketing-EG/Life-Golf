6ce6f6c29edbb31ab956e94dbfd50e3470b1e0ed
Raymond Youssef <rimonomega@gmail.com>
2021-02-24 16:50:47 +0200

    fixed tymon

--- routes/auth.php
+++ routes/auth.php
@@ -18,14 +18,14 @@ Route::group([
     Route::post('refresh', [AuthController::class, 'refresh'])
         ->name('auth.refresh');
 
-    Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
-        ->name('password.email');
-
-    Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])
-        ->name('password.reset');
-
-    Route::post('/reset-password', [NewPasswordController::class, 'store'])
-        ->name('password.update');
+//    Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+//        ->name('password.email');
+//
+//    Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])
+//        ->name('password.reset');
+//
+//    Route::post('/reset-password', [NewPasswordController::class, 'store'])
+//        ->name('password.update');
 
     //Route::post('me', [AuthController::class, 'me']);
 });
