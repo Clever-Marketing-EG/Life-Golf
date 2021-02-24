3656e0a292535c4d17e4f462bd594a635c9f7b84
Raymond Youssef <rimonomega@gmail.com>
2021-02-24 16:37:16 +0200

    done with images controller

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -36,6 +37,17 @@ class ImagesController extends Controller
     }
 
 
+    private function generateRandomString($length = 10): string
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
     /**
      * Save Product image
      *
@@ -44,16 +56,22 @@ class ImagesController extends Controller
      */
     public function products(Request $request): JsonResponse
     {
-        $request->validate([
+        $validated = $request->validate([
             'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:10240',
-            'name' => 'required|string|min:3'
+            'product_id' => 'required|integer|exists:products,id'
         ]);
 
-        $imageName = $request['name'].'_'.time().'.'.$request['image']->extension();
+        $imageName = $this->generateRandomString().'_'.time().'.'.$request['image']->extension();
         $request['image']->storeAs('public/products_images', $imageName);
+
+        $image = new Image();
+        $image['url'] = asset('storage/products_images/'.$imageName);
+        $image['product_id'] = $validated['product_id'];
+        $image->save();
+
         return response()->json([
             "success" => true,
-            'image_url' => asset('storage/products_images/'.$imageName)
+            'data' => $image
         ]);
     }
 
