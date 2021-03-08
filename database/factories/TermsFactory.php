b784a75e41b75d26c8a8101bfa193df0423d547e
Rana Medhat <rana.medhat69@gmail.com>
2021-03-08 11:47:46 +0200

    ..

---
+++ database/factories/TermsFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Terms;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TermsFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Terms::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+            'name' => $this->faker->word,
+            'name_ar' => $this->faker->word,
+            'desc' => $this->faker->sentence,
+            'desc_ar' => $this->faker->sentence
+
+
+        ];
+    }
+}
