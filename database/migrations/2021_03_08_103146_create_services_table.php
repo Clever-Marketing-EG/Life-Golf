bbb89b5e7dad31cdbd150c5652784f8a8aff1603
Raymond Youssef <rimonomega@gmail.com>
2021-03-31 11:31:46 +0200

    fixed services migration

--- database/migrations/2021_03_08_103146_create_services_table.php
+++ database/migrations/2021_03_08_103146_create_services_table.php
@@ -17,8 +17,8 @@ class CreateServicesTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('name_ar');
-            $table->string('description');
-            $table->string('description_ar');
+            $table->text('description');
+            $table->text('description_ar');
             $table->string('title1');
             $table->string('title1_ar');
             $table->string('title2');
