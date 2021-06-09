020ea92cdaba57d7e040e1bb9a026da059e2f8bb
Raymond Youssef <rimonomega@gmail.com>
2021-06-09 15:57:14 +0200

    finish product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Http\Request;
 use Mockery\Exception;
@@ -13,14 +14,17 @@ class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'name',
-        'name_ar',
-    ];
+    protected $guarded = [];
 
 
     protected $with = ['images'];
 
+    protected $casts = [
+        'points' => 'array',
+        'points_ar' => 'array',
+        'features' => 'array',
+        'features_ar' => 'array'
+    ];
 
     /**
      * Get the images for the product.
@@ -35,74 +39,33 @@ class Product extends Model
      * validates product information
      *
      * @param Request $request
-     * @param Product $product
-     * @return Product
+     * @return array
      */
-    public static function validateProduct(Request $request, Product $product): Product
+    public static function validate(Request $request): array
     {
-        $validated = $request->validate([
+        return $request->validate([
             'name' => 'required|min:3|string',
             'name_ar' => 'required|min:3|string',
             'description' => 'required|min:3|string',
             'description_ar' => 'required|min:3|string',
-            'points' =>'required|min:3|string',
-            'points_ar' =>'required|min:3|string',
-            'features' => 'required|min:3|string',
-            'features_ar' => 'required|min:3|string',
-            'image_url'  => 'required|min:3|url',
+            'points' =>'required|array|min:1',
+            'points.*' =>'required|string|min:3',
+            'points_ar' =>'required|array|min:1',
+            'points_ar.*' =>'required|string|min:3',
+            'features' => 'required|array|min:1',
+            'features.*' => 'required|string|min:3',
+            'features_ar' => 'required|array|min:1',
+            'features_ar.*' => 'required|string|min:3',
+            'image_url'  => 'required|url',
             'configuration_image_url' => 'url|nullable',
             'sub_category_id' => 'required|integer|exists:sub_categories,id'
         ]);
 
-
-        $product['name'] = $validated['name'];
-        $product['name_ar'] = $validated['name_ar'];
-        $product['features'] = $validated['features'];
-        $product['features_ar'] = $validated['features_ar'];
-        $product['description'] = $validated['description'];
-        $product['description_ar'] = $validated['description_ar'];
-
-        $pointsArray = explode(',', request('points'));
-        $points = '[';
-        foreach ($pointsArray as $point) {
-            $points .= '"' . $point . '"' . ',';
-        }
-        $points = rtrim($points, ",");
-        $points .= ']';
-        $product['points'] = $points;
-
-        $product['name'] = $validated['name'];
-        $product['name_ar'] = $validated['name_ar'];
-        $product['features'] = $validated['features'];
-        $product['features_ar'] = $validated['features_ar'];
-        $product['description'] = $validated['description'];
-        $product['description_ar'] = $validated['description_ar'];
-
-        $pointsArray = explode(',', request('points_ar'));
-        $points = '[';
-        foreach ($pointsArray as $point) {
-            $points .= '"' . $point . '"' . ',';
-        }
-        $points = rtrim($points, ",");
-        $points .= ']';
-
-        $product['points_ar'] = $points;
-        $product['image_url'] = $validated['image_url'];
-        $product['configuration_image_url'] = $validated['configuration_image_url'];
-        $product['sub_category_id'] = $validated['sub_category_id'];
-
-        $product->save();
-        return $product;
-
     }
 
-    /**
-     * Returns the images of this product
-     *
-     * @return Product
-     */
-    public function getImages()
+
+    public function subCategory(): BelongsTo
     {
-//        return $this->load
+        return $this->belongsTo(SubCategory::class);
     }
 }
